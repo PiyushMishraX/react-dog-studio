c5f1c289062a0fe635886b6f990f58044013b069
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-16 22:09:39 +0530

    Load the model in the scene

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -5,24 +5,39 @@ import React from 'react'
 import { Canvas, useThree } from '@react-three/fiber'
 import { log } from 'three'
 // useHook can only be used the canvas component 
+// all hooks from react three fibre can only be inside canvas
 
-import { OrbitControls  } from '@react-three/drei'
+// import { OrbitControls  } from '@react-three/drei' // orbit controls to give mouse the contoller
+import { OrbitControls , useGLTF } from '@react-three/drei' // useGLTF hook to load and use the model
 
 const Dog = () => {
 
   // gl -<- renderer
-  useThree(({camera,sceme,gl}) =>{
-    console.log(camera.position);
+  // useThree(({camera,sceme,gl}) =>{
+  //   console.log(camera.position);
     
-  })
+  // })
+
+  // return (
+  //   <>
+  //         <mesh>
+  //           <meshBasicMaterial color={0x00FF00} />
+  //           <boxGeometry args={[1,1,1,1]} />
+  //         </mesh>
+  //         <OrbitControls />  {/* WITH ORBIT CNTROLS WE CAN USE MOUSE TO INTERACT WITH THE MESH   , we do not will be using this in the final project but in the development it hekps with debugging etc*/}
+  //   </>
+  // )
+
+
+  //  GIVE PATH RESPECT TO THE PUBLIC FOLDER NOT FROM THE CURRENT FILE LOCATION
+  const model = useGLTF("/models/dog.drc.glb")
 
   return (
     <>
-          <mesh>
-            <meshBasicMaterial color={0x00FF00} />
-            <boxGeometry args={[1,1,1,1]} />
-          </mesh>
-          <OrbitControls />  {/* WITH ORBIT CNTROLS WE CAN USE MOUSE TO INTERACT WITH THE MESH  */}
+      <primitive object={model.scene} position = {[0,0,0]} />
+      <directionalLight position={[ 0, 5, 5]} color={0xFFFFFF} intersity={10}/>
+
+      <OrbitControls />
     </>
   )
 }
